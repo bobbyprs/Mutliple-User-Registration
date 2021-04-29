5bc4643d94174f8996f062e71829bb13fda23cf2
bobbyprs <32490171+bobbyprs@users.noreply.github.com>
2021-04-29 17:35:59 +0530

    Update settings.py

--- multiusers/multiusers/settings.py
+++ multiusers/multiusers/settings.py
@@ -137,8 +137,8 @@ AUTH_USER_MODEL = 'users.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'saikumar3512@gmail.com'
-EMAIL_HOST_PASSWORD = 'gzterugfowwzgsbk'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Virtual Mall Team<no-reply@VirtualMall.com>'
